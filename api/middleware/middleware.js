013fd352137338faa2b57c31474ba86627c82603
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-11 19:57:00 -0400

    made change to middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -21,9 +21,7 @@ async function validateUserId(req, res, next) {
   try {
     const user = await USER.getById(req.params.id);
     if (!user) {
-      res.status(404).json({
-        message: "user not found",
-      });
+      next({ status: 404, message: "user not found" });
     } else {
       req.user = user;
       next();
@@ -48,6 +46,7 @@ function validateUser(req, res, next) {
       message: "missing required name field",
     });
   } else {
+    req.name = name;
     next();
   }
 }
@@ -59,8 +58,15 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log(validatePost);
-  next();
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({
+      message: "missing required text field",
+    });
+  } else {
+    req.text = text;
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
